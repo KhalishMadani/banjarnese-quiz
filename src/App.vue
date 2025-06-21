8ec0fdddbbca8eec1474d93516a0280decedec05
Khalish Madani <93044975+KhalishMadani@users.noreply.github.com>
2025-06-21 14:01:26 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import Reference from './components/Reference.vue';
 </script>
 
 <template>
-  <div class="flex flex-col bg-white py-8 px-6 shadow rounded gap-x-10 gap-y-20">
+  <div class="flex flex-col bg-white py-8 px-6 shadow rounded-xl gap-x-10 gap-y-20">
     <!-- <HelloWorld msg="Vite + Vue" /> -->
      <div class="">
        <Header/>
