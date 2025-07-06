4a576ccd832b9c02fda98d965aab32a2618f87db
Khalish Madani <93044975+KhalishMadani@users.noreply.github.com>
2025-07-06 14:28:21 +0700

    pwa integrations

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,28 @@ export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Vue PWA',
+        short_name: 'VuePWA',
+        description: 'My Progressive Web App',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,svg}'],
+      },
+    }),
   ],
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  preview: {
+    host: 'localhost',
+    port: 4000,
+  }
 })
